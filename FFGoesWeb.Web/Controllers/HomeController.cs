6d11727bab32674a7f438156f55a79cd90c38573
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-02-04 06:36:36 +0200

    Initial State of the Project
    Cleared useless includes in Layout.
    Created a basic Controller and a couple of Views

---
+++ FFGoesWeb.Web/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿namespace FinalFantasyTryoutGoesWeb.Controllers
+{
+    using FinalFantasyTryoutGoesWeb.Data;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Linq;
+
+    public class HomeController : Controller
+    {
+        private readonly FFDbContext context = new FFDbContext();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("Home/About")]
+        [Route("Home/About")]
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
