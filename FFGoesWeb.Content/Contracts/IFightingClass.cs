fcbff0071b699e8993fc80472e7413db2dd27c09
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-02-06 14:03:26 +0200

    Added Additional Models and Contracts

---
+++ FFGoesWeb.Content/Contracts/IFightingClass.cs
@@ -0,0 +1,16 @@
+﻿namespace FinalFantasyTryoutGoesWeb.GameContent.Contracts
+{
+    public interface IFightingClass
+    {
+        string ClassType { get; }
+        double MaxHP { get; }
+        int HealthRegen { get; }
+        double MaxMana { get; }
+        int ManaRegen { get; }
+        double AttackPower { get; }
+        double ArmorValue { get; }
+        double RessistanceValue { get; }
+        double MagicPower { get; }
+        double CritChance { get; }
+    }
+}
---
+++ FFGoesWeb.Content/Equipment/Armor.cs
@@ -0,0 +1,25 @@
+﻿namespace FinalFantasyTryoutGoesWeb.GameContent.Equipment
+{
+    using FinalFantasyTryoutGoesWeb.Data.Entities;
+
+    public class Armor : Item
+    {
+        public Armor(string name,string slot, int level
+            , string classType, int stamina
+            , int strength, int intellect, int agility
+            , int spirit, double armorValue, double ressistanceValue)
+        {
+            this.Name = name;
+            this.Slot = slot;
+            this.Level = level;
+            this.ClassType = classType;
+            this.Stamina = stamina;
+            this.Strength = strength;
+            this.Intellect = intellect;
+            this.Agility = agility;
+            this.Spirit = spirit;
+            this.ArmorValue = armorValue;
+            this.RessistanceValue = ressistanceValue;
+        }
+    }
+}
