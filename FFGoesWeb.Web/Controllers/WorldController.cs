a43295bdff25db2352c5a52a4175a07ff9d8cfd7
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-02-10 14:38:06 +0200

    Added WorldController + Relative Views

---
+++ FFGoesWeb.Web/Controllers/WorldController.cs
@@ -0,0 +1,50 @@
+﻿namespace FinalFantasyTryoutGoesWeb.Controllers
+{
+    using FinalFantasyTryoutGoesWeb.Data;
+    using FinalFantasyTryoutGoesWeb.Data.Entities;
+    using FinalFantasyTryoutGoesWeb.GameContent.Utilities.Generators;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class WorldController : Controller
+    {
+        private static readonly FFDbContext context = new FFDbContext();
+        private readonly Unit player = context.Units.FirstOrDefault();
+        private readonly Random rng = new Random();
+        private readonly TreasureGenerator treasureGenerator = new TreasureGenerator();
+
+        [HttpGet("World/Home")]
+        [Route("World/Home")]
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpGet("World/Explore")]
+        [Route("World/Explore")]
+        public IActionResult Explore()
+        {
+            int exploreNumber = rng.Next(0, 10);
+            if (exploreNumber >= 0 && exploreNumber <= 10)
+            {
+                return View(@"\EnemyEncounter");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet("World/TreasureLoot")]
+        [Route("World/TreasureLoot")]
+        public async Task<IActionResult> TreasureEncounter(string option)
+        {
+            option = Request.Query["option"];
+            await context.SaveChangesAsync();
+
+            return View(treasureGenerator.Generate(player,rng,option));
+        }
+    }
+}
