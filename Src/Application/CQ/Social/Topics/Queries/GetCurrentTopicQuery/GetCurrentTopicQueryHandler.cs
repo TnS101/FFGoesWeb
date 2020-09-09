708c42f34d22b09232385b3001e7579ce99fecfe
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-09-09 22:23:01 +0300

    Clean Project

--- Src/Application/CQ/Social/Topics/Queries/GetCurrentTopicQuery/GetCurrentTopicQueryHandler.cs
+++ Src/Application/CQ/Social/Topics/Queries/GetCurrentTopicQuery/GetCurrentTopicQueryHandler.cs
@@ -1,6 +1,5 @@
 ﻿namespace Application.CQ.Social.Topics.Queries.GetCurrentTopicQuery
 {
-    using System.Collections.Generic;
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
